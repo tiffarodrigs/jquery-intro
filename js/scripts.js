c68dc2146072bff3040b7aff5bcae4992c446a06
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-10 14:05:18 -0800

    Add jquery intro project files
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+  jQuery("h1").click(function(){
+    alert("This is a heading");
+    alert("I told you, THIS IS A HEADING!");
+  });
+
+  jQuery("p").click(function(){
+    alert("This is a para");
+  });
+
+  jQuery("img").click(function(){
+    alert("This ia an image");
+  });
+});
